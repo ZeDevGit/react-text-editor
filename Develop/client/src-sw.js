7a156ab1515a3a98d2561852a50d43931174f6db
Ze <elawrence.bda@gmail.com>
2024-05-14 01:20:38 -0400

    Deleted Develop folder and restarted. Fixed files to work correctly

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,20 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // Add a filter to cache only requests for styles, scripts, and images.
+  ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'image',
+  // Use a CacheFirst strategy with a cache name of 'image-cache'.
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -6,25 +6,55 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js', // Entry point for the main bundle
+    install: './src/js/install.js' // Entry point for the install bundle
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Path to index.html template
+      filename: 'index.html', // Output filename (defaults to index.html)
+    }),
+    new WebpackPwaManifest({
+      name: 'My PWA Text Editor',
+      short_name: 'Text Editor',
+      description: 'A text editor Progressive Web Application',
+      background_color: '#ffffff',
+      theme_color: '#2196f3',
+      icons: [
+        {
+          src: path.resolve('./src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'sw.js',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ],
-
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+  },
 };
